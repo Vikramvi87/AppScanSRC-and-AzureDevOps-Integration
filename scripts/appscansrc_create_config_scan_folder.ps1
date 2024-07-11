11314f3c6dc830f8a4ac73c051040b1a8eded9be
Vikram Vijan <78088425+Vikramvi87@users.noreply.github.com>
2024-07-11 12:24:34 -0400

    Update appscansrc_create_config_scan_folder.ps1

--- scripts/appscansrc_create_config_scan_folder.ps1
+++ scripts/appscansrc_create_config_scan_folder.ps1
@@ -26,7 +26,7 @@ if ($env:compiledArtifactFolder -ne "none"){
   write-output "of `"$env:WorkingDirectory\$env:compiledArtifactFolder`"" >> script.scan
   write-output "sc `"$env:aseAppName-$env:BuildNumber.ozasmt`" -scanconfig `"$env:scanConfig`" -name `"$env:aseAppName-$env:BuildNumber`"" >> script.scan
   write-output "sc `"$env:aseAppName-$env:BuildNumber.ozasmt`" -scanconfig `"$env:scanConfig`" -name `"$env:aseAppName-$env:BuildNumber`"" >> script.scan
-  write-output "report Findings pdf-detailed `"$env:aseAppName-$env:BuildNumber.pdf`" `"$env:aseAppName-$env:BuildNumber.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
+  # write-output "report Findings pdf-detailed `"$env:aseAppName-$env:BuildNumber.pdf`" `"$env:aseAppName-$env:BuildNumber.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
   write-output "pa `"$env:aseAppName-$env:BuildNumber.ozasmt`"" >> script.scan
   write-output "pase `"$env:aseAppName-$env:BuildNumber.ozasmt`" -aseapplication $env:aseAppName -name `"$env:aseAppName-$env:BuildNumber`"" >> script.scan
   write-output "logout" >> script.scan
@@ -38,7 +38,7 @@ else{
   write-output "of `"$env:WorkingDirectory`"" >> script.scan
   write-output "sc `"$env:aseAppName-$env:BuildNumber.ozasmt`" -scanconfig `"$env:scanConfig`" -name `"$env:aseAppName-$env:BuildNumber`" -sourcecodeonly true" >> script.scan
   write-output "sc `"$env:aseAppName-$env:BuildNumber.ozasmt`" -scanconfig `"$env:scanConfig`" -name `"$env:aseAppName-$env:BuildNumber`" -sourcecodeonly true" >> script.scan
-  write-output "report Findings pdf-detailed `"$env:aseAppName-$env:BuildNumber.pdf`" `"$env:aseAppName-$env:BuildNumber.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
+  # write-output "report Findings pdf-detailed `"$env:aseAppName-$env:BuildNumber.pdf`" `"$env:aseAppName-$env:BuildNumber.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
   write-output "pase `"$env:aseAppName-$env:BuildNumber.ozasmt`" -aseapplication $env:aseAppName -name `"$env:aseAppName-$env:BuildNumber`"" >> script.scan
   write-output "pa `"$env:aseAppName-$env:BuildNumber.ozasmt`"" >> script.scan
   write-output "logout" >> script.scan
