7a67853e0df2bfa2f5337601fae3f31eb7aa0886
Vikram Vijan <78088425+Vikramvi87@users.noreply.github.com>
2024-07-11 09:56:45 -0400

    Update appscansrc_create_config_scan_folder.ps1

--- scripts/appscansrc_create_config_scan_folder.ps1
+++ scripts/appscansrc_create_config_scan_folder.ps1
@@ -29,6 +29,7 @@ if ($env:compiledArtifactFolder -ne "none"){
   write-output "report Findings pdf-detailed `"$env:aseAppName-$env:BuildNumber.pdf`" `"$env:aseAppName-$env:BuildNumber.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
   write-output "pa `"$env:aseAppName-$env:BuildNumber.ozasmt`"" >> script.scan
   write-output "exit" >> script.scan
+  write-output "exit" >> script.scan
   
   write-host "Config file created for compiled folder $env:WorkingDirectory\$env:compiledArtifactFolder."
 }
@@ -39,6 +40,7 @@ else{
   write-output "sc `"$env:aseAppName-$env:BuildNumber.ozasmt`" -scanconfig `"$env:scanConfig`" -name `"$env:aseAppName-$env:BuildNumber`" -sourcecodeonly true" >> script.scan
   write-output "report Findings pdf-detailed `"$env:aseAppName-$env:BuildNumber.pdf`" `"$env:aseAppName-$env:BuildNumber.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
   write-output "pa `"$env:aseAppName-$env:BuildNumber.ozasmt`"" >> script.scan
+  write-output "exit" >> script.scan
   write-output "exit" >> script.scan
   
   write-host "Config file created (source code only scan)."
